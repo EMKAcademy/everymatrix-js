bd844b4f1c4b4b3df5c4a6031684f9c2bac0f353
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2019-11-18 18:26:19 +0200

    tema p2

--- M01S04/ex04/app.js
+++ M01S04/ex04/app.js
@@ -1,9 +1,25 @@
 var i;
 
-for (i = 1; i<=100; i++) {
-  if (i % 5 !== 0) {
-    continue;
-  }
-  console.log(i)
+// for (i = 1; i<89; i++) {
 
+//   if(i===42){
+//     break;
+//   }
+//   console.log(i)
+
+// }
+
+// for (i = 1; i<89; i++) {
+
+//   if(i%2!==0){
+    
+ 
+//   console.log(i)
+// }
+// }
+
+for (i=1; i<89; i++){
+  if(i%3===0){
+    console.log(i);
+  }
 }
