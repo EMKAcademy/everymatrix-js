3e297e5ae0d9dedd71fea32a5720e77c0dfea0a3
EMKAcademy <48829574+EMKAcademy@users.noreply.github.com>
2019-11-25 20:01:23 +0200

    Tema nu e tema , daca nu e tema facuta !

--- Proiect/js/app.js
+++ Proiect/js/app.js
@@ -3,21 +3,35 @@ var form=document.getElementById('game');
 var option=document.getElementById('options');
 var difficulty=document.getElementById('difficulty');
 var generatedNumber=0;
+var usedNumbers=[];
+var time = 0;
 // console.log(level);
 var difficultyMap = {
   easy: 10,
   medium: 100,
   hard: 1000
 };
-var selectedDifficulty=difficultyMap['easy']
+var selectedDifficulty=difficultyMap['easy'];
 
+function timer(){
+  time=time-1
+  console.log(time)
+  let rahat = document.getElementById('timebar');
+  rahat.style.width=time+'%';
+  if(time === 0){
+  alert('Timpul a expirat')
+  startGame(selectedDifficulty);
+  }
+}
 
 
 function startGame(difLevel) {
+  time=100;
+  setInterval(timer,1000);
   generatedNumber=Math.ceil(Math.random()*difLevel);
   selectedDifficulty=difLevel;
   console.log(generatedNumber);
-
+  
 }
 
 
@@ -34,6 +48,9 @@ option.addEventListener('submit', function(evt){
 
 form.addEventListener('submit', function(evt){
   inputNumber=inputElement.value;
+  usedNumbers.push(inputNumber);
+  usedN=document.getElementById('usedNumbers');
+  usedN.innerText = usedNumbers;
   if(inputNumber>generatedNumber){
     alert('Numarul introdus e mai mare.')
   }
@@ -56,5 +73,7 @@ form.addEventListener('reset', function(evt){
   startGame(selectedDifficulty);
   evt.preventDefault()
 })
+
+
 
 startGame(selectedDifficulty);
