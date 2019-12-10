588556f9544a5d039af7cab9e7cf088ceaa41b9e
Catalin Curelea <catalin.curelea@everymatrix.com>
2019-12-10 20:02:54 +0200

    m02

--- M02S01/ex01/app.js
+++ M02S01/ex01/app.js
@@ -1,4 +1,8 @@
 class Car {
+  topSpeed = 160;
+  topReverseSpeed = -50;
+  isTrunkOpen = false;
+  areLightson = false;
   constructor(
     make, 
     color, 
@@ -18,6 +22,71 @@ class Car {
   decelerate() {
     this.speed--;
   }
+  stop() {
+    this.speed = 0;
+    console.log(this.speed);
+  }
+  setSpeed(speed) {
+    if (speed > this.topSpeed){
+      this.speed = this.topSpeed;
+      return;
+    }
+    if (speed < this.topReverseSpeed) {
+      this.speed = this.topReverseSpeed;
+      return;
+    }
+    this.speed = speed;
+    return speed;
+  }
+  openTrunk() {
+    if(this.isTrunkOpen){
+      alert('already open')
+      return;
+    }
+    this.isTrunkOpen = true;
+  }
+  closeTrunk() {
+    if(!this.isTrunkOpen){
+      alert('already closed')
+      return;
+    }
+    this.isTrunkOpen = false;
+  }
+  turnLightsOn() {
+    if(this.areLightsOn){
+      console.log('already on')
+      return;
+    }
+    this.areLightson = true;
+  }
+  turnLightsOff() {
+    if(!this.areLightson){
+     console.log('already off')
+      return;
+    }
+    this.areLightson = false;
+  }
+  flashLights() {
+    this.turnLightsOff();
+    this.turnLightsOn();
+    window.setTimeout(() => {
+      this.turnLightsOff()}, 5000)
+
+
+  }
 }
 
 var audi = new Car('Audi', 'black', 4, 50);
+let opel = new Car('Opel','red', 4, 3);
+
+let cars = [audi, opel];
+
+cars.forEach((car) => {
+  console.log(`Masina era marca ${car.make} si se deplasa cu viteza ${car.speed}`)
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+  console.log(`Viteza noua este ${car.speed}`)
+});
