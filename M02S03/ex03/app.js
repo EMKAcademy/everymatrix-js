95f0b82acf4d1e5ae99bc7d2f400811a08fc52b5
Catalin Curelea <catalin.curelea@everymatrix.com>
2019-12-17 19:59:20 +0200

    easy stuff

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,15 @@
+let stage = document.querySelector('.stage');
+let message = document.querySelector('.message');
+let countElement = document.querySelector('.count')
+let count = 0;
 
+stage.addEventListener('mouseover', () => {
+  message.innerText = 'Mouse-ul este pe scena'; 
+  count = count + 1;
+
+});
+
+stage.addEventListener('mouseout', ()=> {
+  message.innerText = 'Mouse-ul nu este pe scena';
+  countElement.innerText=`Mouse-ul a fost pe scena de ${count} ori`
+})
