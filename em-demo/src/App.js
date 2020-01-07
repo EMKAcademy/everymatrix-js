0cba55ebbb3c4352a28b82f33081581ef2b9b8d5
Catalin Curelea <catalin.curelea@everymatrix.com>
2020-01-07 19:59:34 +0200

    react

---
+++ em-demo/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route
+ } from 'react-router-dom';
+import { Home } from './Pages/Home/home';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Dragos',
+      surname: 'Iordache',
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <br></br>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home</h1>
+            <Home name={`${this.state.name} ${this.state.surname}`}></Home>
+          </Route>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
